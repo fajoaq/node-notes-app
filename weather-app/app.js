411820adf4b1ac056d8bde0243ec47a56b6164e2
Francis J <fajoaq@gmail.com>
2021-04-11 01:21:31 -0400

    added geocoding api, with some error handling

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,10 +2,33 @@ const request = require('postman-request');
 const API_KEY = require('../api/weatherstack.js')
 const chalk = require('chalk');
 
-const url = `http://api.weatherstack.com/current?access_key=${API_KEY}&query=37.8267,-122.4233&units=f`
+const weatherUrl = `http://api.weatherstack.com/current?access_key=${API_KEY}&query=37.8267,-122.4233&units=f`
 
-request({ url, json: true }, (error, res) => {
-    const { temperature, feelslike, weather_descriptions } = res.body.current;
-    console.log(`It is currently ${weather_descriptions[0].toLowerCase()}, ${chalk.blue(temperature)} degrees out.`);
-    console.log(`It feels like ${chalk.blue(feelslike)} degrees.`);
+request({ url: weatherUrl, json: true }, (error, res) => {
+    if(error) {
+        console.log('Unable to connect to weather service.');
+
+    } else if (res.body.error) {
+        console.log('Unable to find a location.')
+    }
+    else {
+        const { temperature, feelslike, weather_descriptions } = res.body.current;
+        console.log(`It is currently ${weather_descriptions[0].toLowerCase()}, ${chalk.blue(temperature)} degrees out.`);
+        console.log(`It feels like ${chalk.blue(feelslike)} degrees.`);
+    }
 });
+
+
+const geocodingUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiZmFqb2FxIiwiYSI6ImNrbmNudXdieTF1NTQyb296cWVmbDJlbzIifQ.xcbVqKXvrUSWXs_USbb-2A&limit=1';
+
+request({ url: geocodingUrl, json: true }, (error, res) => {
+    if(error) {
+        console.log('Unable to connect to geocoding service.')
+    } else if(res.body.features.length === 0) {
+        console.log('No matching results.')
+    } else {
+        const { center } = res.body.features[0];
+        console.log(`longitude: ${chalk.green(center[0])}`);
+        console.log(`latitute: ${chalk.green(center[1])}`);
+    }
+}); 
