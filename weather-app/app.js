4d010ffcd1fffc84b03dec02649b45a32535539b
Francis J <fajoaq@gmail.com>
2021-04-14 05:40:09 -0400

    Chained callbacks for geocode and forecast

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,12 +1,13 @@
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-geocode('philadelphia', (error, data) => {
-    if(error) console.log('Error: ', error)
-    if(data) console.log('Data: ', data)
+geocode('Porto Portugal', (error, data) => {
+    if(error){
+        console.log('Error: ', error)
+    } else if(data) {
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if(error) console.log('Error: ', error)
+            else if(forecastData) console.log('Data: ', data.place_name, forecastData)
+        });   
+    }
 });
- 
-forecast(44.1545, -75.7088, (error, data) => {
-    if(error) console.log('Error: ', error)
-    if(data) console.log('Data: ', data)
-})
